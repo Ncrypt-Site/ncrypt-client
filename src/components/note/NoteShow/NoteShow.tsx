6d4f985af4b6b80ab5dd4be18456983701e47523
Milad Dehghan <milad.d3@gmail.com>
2020-05-26 01:07:25 +0430

    note password

--- src/components/note/NoteShow/NoteShow.tsx
+++ src/components/note/NoteShow/NoteShow.tsx
@@ -4,20 +4,17 @@ import mangane from '../../../assets/mangane.svg'
 
 interface NoteShowProps {
   text?: string
+  blur?: boolean
 }
 
-export const NoteShow: React.FC<NoteShowProps> = ({text,children}) => {
+export const NoteShow: React.FC<NoteShowProps> = ({ text, children, blur }) => {
   return (
     <div className="main-note">
-      <div className="note-container">
+      <div className={`note-container ${blur ? 'blur' : ''}`}>
         <div className="mangane">
           <img src={mangane} alt="" />
         </div>
-        <div
-          className="note-show"
-        >
-          {text || children}
-        </div>
+        <div className="note-show">{text || children}</div>
       </div>
     </div>
   )
--- src/components/pages/Note/Note.tsx
+++ src/components/pages/Note/Note.tsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import './note.scss'
+import React, { useState, ChangeEvent } from 'react'
 import { NoteShow } from '../../note/NoteShow/NoteShow'
+import { NotePassword } from '../../note/NotePassword/NotePassword'
+import { NyButton } from '../../shared/NyButton/NyButton'
+import Icon from '@mdi/react'
+import { mdiLock } from '@mdi/js'
 
 interface NoteProps {}
 
 export const Note: React.FC<NoteProps> = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [password, setPassword] = useState('')
+
+  const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target
+
+    setPassword(input.value)
+  }
+
   return (
-    <div>
-      <NoteShow>
+    <div className="note-page">
+      <NoteShow blur>
         This HTML file is a template. If you open it directly in the browser,
         you will see an empty page. You can add webfonts, meta tags, or
         analytics to this file. The build step will place the bundled scripts
         into the tag. To begin the development, run `npm start` or `yarn start`.
         To create a production bundle, use `npm run build` or `yarn build`.
       </NoteShow>
+
+      <div className="password-box">
+        <NotePassword
+          placeholder="Enter Password"
+          label="Enter the password to open the Note"
+          onChange={passwordHandler}
+        />
+
+        <NyButton>
+          <Icon path={mdiLock} size="1rem" style={{ marginRight: '0.25rem' }} />
+          Open Note
+        </NyButton>
+      </div>
     </div>
   )
 }
