42242dc6963de2c365d4241d5676d87287442c32
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-31 17:15:50 +0200

    Add decrypt function to Crypto helper

--- src/helpers/Crypto/Crypto.ts
+++ src/helpers/Crypto/Crypto.ts
@@ -7,6 +7,10 @@ interface SecureNote {
   Note: string
 }
 
+interface Note {
+  Note: string
+}
+
 export function encryptNote(note: string): SecureNote {
   const key = CryptoJS.enc.Utf8.parse(GenerateRandomString(32))
   const iv = CryptoJS.enc.Utf8.parse(GenerateRandomString(16))
@@ -21,5 +25,20 @@ export function encryptNote(note: string): SecureNote {
     Key: CryptoJS.enc.Base64.stringify(key),
     Iv: CryptoJS.enc.Base64.stringify(key),
     Note: encryptedNote.toString()
+  }
+}
+
+export function decryptNote(note: string, key: string, iv: string): Note {
+  key = CryptoJS.enc.Base64.parse(key)
+  iv = CryptoJS.enc.Base64.parse(iv)
+
+  const decryptedMessage = CryptoJS.AES.decrypt(note, key, {
+    mode: CryptoJS.mode.CTR,
+    iv: iv,
+    padding: CryptoJS.pad.NoPadding
+  })
+
+  return {
+    Note: decryptedMessage.toString(CryptoJS.enc.Utf8)
   }
 }
