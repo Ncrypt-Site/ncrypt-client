a9605b7c2c206ab6a8f61ec6c9c6c5c7cddf9364
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-31 19:01:51 +0200

    Update test encrypt/decrypt note according to the encrypt method signature change

--- src/helpers/Crypto/Crypto.test.ts
+++ src/helpers/Crypto/Crypto.test.ts
@@ -1,19 +1,11 @@
 import { encryptNote, decryptNote } from './Crypto'
 
 test('It can encrypt and decrypt a note', () => {
-  // mock window.crypto (not sure the best approach but works)
-  const crypto = require('crypto');
-  // @ts-ignore
-  Object.defineProperty(global.self, 'crypto', {
-    value: {
-      // @ts-ignore
-      getRandomValues: arr => crypto.randomBytes(arr.length)
-    }
-  });
-
+  const key = 'Yh5rEdo2W75zl3qfYh5rEdo2W75zl3qf'
+  const iv = 'Yh5rEdo2W75zl3qf'
   const note = 'I don’t like the idea that I’m not in control of my life.'
 
-  const secureNote = encryptNote(note)
+  const secureNote = encryptNote(note, key, iv)
   const decryptedNote = decryptNote(secureNote.Note, secureNote.Key, secureNote.Iv)
 
   expect(decryptedNote.Note).toBe(note)
