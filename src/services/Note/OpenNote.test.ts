17f56551e636570d08d85c399d178a691438d4d5
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-31 22:52:12 +0200

    Add a test for open note process

---
+++ src/services/Note/OpenNote.test.ts
@@ -0,0 +1,23 @@
+import { OpenNote } from './OpenNote'
+import { CreateNote } from './CreateNote'
+
+test("It will open an encrypted note",()=>{
+  // mock window.crypto (not sure the best approach but works)
+  const crypto = require('crypto');
+  // @ts-ignore
+  Object.defineProperty(global.self, 'crypto', {
+    value: {
+      // @ts-ignore
+      getRandomValues: arr => crypto.randomBytes(arr.length)
+    }
+  });
+
+
+  const message = "You have a problem with authority, Mr. Anderson. You believe you are special," +
+    " that somehow the rules do not apply to you. Obviously, you are mistaken."
+  const encrypted = CreateNote(message)
+
+  const note = OpenNote(encrypted.Note, encrypted.Key)
+
+  expect(note.Note).toBe(message)
+})
