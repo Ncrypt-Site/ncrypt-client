95597aa0e7cf4561430fbb3ea1f101c98c529958
Milad Dehghan <milad.d3@gmail.com>
2020-06-06 03:39:42 +0430

    upgrade packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './scss/App.scss'
 import { MainLayout } from './components/layout/MainLayout'
 import { Index } from './components/pages'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout Content={Index} />
+      <ToastContainer />
     </BrowserRouter>
   )
 }
