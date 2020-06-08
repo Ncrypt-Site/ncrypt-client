ddb5d0e0a987c88707e7cb0a635fc1a2c20b6254
Milad Dehghan <milad.d3@gmail.com>
2020-06-09 00:53:24 +0430

    update tests

--- src/components/layout/MainHeader/MainHeader.test.tsx
+++ src/components/layout/MainHeader/MainHeader.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Header } from './MainHeader'
+import { BrowserRouter } from 'react-router-dom'
 
 test('render Header', () => {
-  const { getByAltText } = render(<Header />)
+  const { getByAltText } = render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
   const LogoElement = getByAltText(/nyCrypt/i)
   expect(LogoElement).toBeVisible()
 })
