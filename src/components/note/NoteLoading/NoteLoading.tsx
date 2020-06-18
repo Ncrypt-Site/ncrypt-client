43b353d00d23b9b5e66c7e76e243936321cdf44e
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-18 10:25:43 +0200

    Fix an issue with typescript linter

--- src/components/note/NoteLoading/NoteLoading.tsx
+++ src/components/note/NoteLoading/NoteLoading.tsx
@@ -2,7 +2,7 @@ import './note-loading.scss'
 import React from 'react'
 import gridColored from '../../../assets/grid-colored.svg'
 
-export const NoteLoading: React.FC = ({}) => {
+export const NoteLoading: React.FC = () => {
   return (
     <div className="note-loading">
       <img className="loading-image" src={gridColored} alt="loading..." />
